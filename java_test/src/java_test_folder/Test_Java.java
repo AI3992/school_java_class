a17099e8c2a98aed5391270d83e48eb745f52f77
USER <USER@192.168.1.107>
2022-04-15 10:38:53 +0900

    test_commit

---
+++ java_test/src/java_test_folder/Test_Java.java
@@ -0,0 +1,29 @@
+package java_test_folder;
+
+public class Test_Java {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 5;
+		int num2 = 10;
+		
+		int result = num1 & num2;
+		
+		System.out.println(result);
+		
+		num1 = 5;
+		num2 = 10;
+		
+		result = num1 | num2;
+		System.out.println(result);
+		
+		result = num1 ^ num2;
+		
+		System.out.println(result);
+		
+		int num = 10;
+		result = ~num;
+		System.out.println(result);
+	}
+
+}
